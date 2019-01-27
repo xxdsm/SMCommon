9b1a75da7a33c199728a9d773dbd29df856db081
yeild_eclipse <935057137@qq.com>
2019-01-27 19:54:44 +0800

    增加plc数据

--- src/main/java/com/qs/screen/SMCommon/ld/LDInitInfo.java
+++ src/main/java/com/qs/screen/SMCommon/ld/LDInitInfo.java
@@ -1,11 +1,13 @@
 package com.qs.screen.SMCommon.ld;
 
+import java.io.Serializable;
+
 import com.qs.screen.SMCommon.SMUser;
 
 /**
  *设备初始化配置信息
  */
-public class LDInitInfo {
+public class LDInitInfo implements Serializable {
 	public int rmd_id;
 	public SMUser user;
 	public String areacode;
