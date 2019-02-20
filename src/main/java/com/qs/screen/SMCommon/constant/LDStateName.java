2f5bb42f39d798caa339ab7f47a76ccf1180ccc6
yeild_eclipse <935057137@qq.com>
2019-02-20 17:02:24 +0800

    增加状态类型

---
+++ src/main/java/com/qs/screen/SMCommon/constant/LDStateName.java
@@ -0,0 +1,11 @@
+package com.qs.screen.SMCommon.constant;
+
+public interface LDStateName {
+	String STATE_HDMI = "hdmi";
+	String STATE_EST = "est";
+	String STATE_CTL = "ctl";
+	String STATE_LCD_ON = "lcd_on";
+	String STATE_LIGHT_ON = "light_on";
+	String STATE_FAN_ON = "fan_on";
+	String STATE_LOCK = "lock";
+}
