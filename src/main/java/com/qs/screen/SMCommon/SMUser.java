06160cf53f35b0a4683f90af13e01dc012bd11c6
yeild_eclipse <935057137@qq.com>
2019-02-21 17:03:04 +0800

    设备配置信息增加比较方法

--- src/main/java/com/qs/screen/SMCommon/SMUser.java
+++ src/main/java/com/qs/screen/SMCommon/SMUser.java
@@ -7,4 +7,14 @@ public class SMUser {
 	public int id;
 	public String username;
 	public String password;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof SMUser)) return false;
+		if (this == obj) return true;
+		SMUser eqUser = (SMUser) obj;
+		if(this.id == eqUser.id) return true;
+		if(this.username == eqUser.username || this.username.equals(eqUser.username)) return true;
+		return false;
+	}
 }
--- src/main/java/com/qs/screen/SMCommon/ld/LDInitInfo.java
+++ src/main/java/com/qs/screen/SMCommon/ld/LDInitInfo.java
@@ -13,4 +13,13 @@ public class LDInitInfo implements Serializable {
 	public String areacode;
 	public int com_id;
 	public int liv_id;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof LDInitInfo)) return false;
+		LDInitInfo eqInfo = (LDInitInfo) obj;
+		if(this.user != null && !this.user.equals(eqInfo.user)) return false;
+		if(this.areacode != null && !this.areacode.equals(eqInfo.areacode)) return false;
+		return this.com_id == eqInfo.com_id && this.liv_id == eqInfo.liv_id;
+	}
 }
